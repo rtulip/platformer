70d0706301c01635908c78c0d813c8c503cd01ee
Robbie <robert.tulip97@gmail.com>
2019-01-02 00:58:15 -0800

    Created player struct

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,95 @@
-extern crate piston_window;
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
 
-use piston_window::*;
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
 
-fn main() {
-    
-    let mut window: PistonWindow = 
-        WindowSettings::new("New Window", [600,600])
-        .exit_on_esc(true).build().unwrap();
-    while let Some(event) = window.next() {
+pub struct App {
+    gl: GlGraphics,     // OpenGL drawing backend.
+    player: Player      // Player struct.
+}
 
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        
+        self.gl.draw(args.viewport(), |_c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+        });
+
+        self.player.render(args)
+    }
+}
+
+pub struct Player {
+
+    gl: GlGraphics,
+    rotation: f64,
+    x: f64,
+    y: f64
+
+}
+
+impl Player {
+    pub fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = (args.width / 2.0,
+                      args.height / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform.trans(x, y)
+                                       .rot_rad(rotation)
+                                       .trans(-25.0, -25.0);
+
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
         });
 
     }
+}
 
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+            "spinning-square",
+            [200, 200]
+        )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        player: Player {
+            gl: GlGraphics::new(opengl),
+            rotation: 0.0,
+            x: 0.0,
+            y: 0.0
+        }
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+    }
 }
